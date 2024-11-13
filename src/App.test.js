2d50a9755a9820b3c78aaacee4e413812e5e2ebb
Minjoon Kim <annawa678@gmail.com>
2024-11-13 10:01:58 +0900

    feat : footer-회사소개버튼 제작

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,13 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
+test("renders learn react link", () => {
+  render(<App />);
+  const linkElement = screen.getByText(/learn react/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
+test("푸터 회사소개 버튼 제작", () => {
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
